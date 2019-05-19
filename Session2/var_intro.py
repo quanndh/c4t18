b49f7a44d3a7cf319c46e2c0f29c2d4c4baadcc6
quannguyennn <wantmorexx9x@gmail.com>
2019-05-19 17:18:25 +0700

    session2

---
+++ Session2/var_intro.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+for i in range(1000):
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    left(0.5)
+    speed(-1)
+mainloop()
